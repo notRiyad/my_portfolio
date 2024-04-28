1b790e406fac033fa83b6e52d42d195063526cdf
Riyad El Yacoubi <riyad.el-yacoubi@epita.fr>
2024-04-28 16:16:21 +0200

    feat(add): redirect to site

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'notRiyad.github.io/my_portfolio/',
+  homepage: 'https://notRiyad.github.io/my_portfolio/',
   title: 'RE.',
 }
 
