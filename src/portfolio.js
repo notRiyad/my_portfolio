ae8f48f1bff0a335e4e3d537e39120dc197cb238
Riyad El Yacoubi <riyad.el-yacoubi@epita.fr>
2024-04-28 15:52:29 +0200

    feat(last): changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'notRiyad.github.io',
+  homepage: 'notRiyad.github.io/my_portfolio/',
   title: 'RE.',
 }
 
